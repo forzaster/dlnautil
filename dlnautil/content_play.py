d5ac73c056146fd169ab389a89b856701c113c6b
forzaster <forzaster@gmail.com>
2022-04-03 20:50:25 +0900

    Implement get renderer info

--- dlnautil/content_play.py
+++ dlnautil/content_play.py
@@ -1,9 +1,54 @@
 import argparse
-import requests
 import logging
+import requests
+import re
+
+from typing import List
+from urllib.parse import urlparse
+from xml.etree import ElementTree
+
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger('content_play')
+
+
+def _parse_xml(node: ElementTree, root_namespace: str, is_retrieve: bool = False) -> List:
+    ret = {}
+    child_ret = []
+    extract_tags = ['deviceType', 'friendlyName', 'manufacturer', 'modelName', 'modelNumber']
+    for c in node:
+        real_tag = c.tag.replace(f'{{{root_namespace}}}', '')
+        # print(f'{real_tag} : {c.attrib} : {c.text}')
+        is_retrieve_child = True if real_tag == 'service' else False
+        # print(f'is_retrieve={is_retrieve_child}')
+        child_ret.extend(_parse_xml(c, root_namespace, is_retrieve_child))
+
+        if is_retrieve or real_tag in extract_tags:
+            ret[real_tag] = c.text
+
+    if ret:
+        child_ret.append(ret)
+
+    return child_ret
+
+
+def _get_root_namespace(et: ElementTree) -> str:
+    m = re.match('{(.*)}(.*)', et.tag)
+    if m:
+        ns = m.group(1)
+        return ns
+
+    return None
+
+
+def _add_url_to_rendererinfo(url: str, infos: List) -> List:
+    urlinfo = urlparse(url)
+    for info in infos:
+        _logger.debug(f'{info}')
+        controlURL = info.get('controlURL')
+        if controlURL:
+            info['url'] = f'{urlinfo.scheme}://{urlinfo.netloc}{controlURL}'
+    return infos
 
 
 def _request(url: str, action: str, data: str):
@@ -29,6 +74,21 @@ def _request(url: str, action: str, data: str):
         _logger.info(f'{action} OK')
     else:
         _logger.error(f'{action} Error : {ret.status_code}')
+
+
+def get_rendererinfo(url: str):
+    headers = {'Content-Type': "text/xml; charset=utf-8"}
+    ret = requests.get(url, headers=headers)
+
+    if ret.status_code == 200:
+        # print(ret.text)
+        et = ElementTree.fromstring(ret.text)
+        root_ns = _get_root_namespace(et)
+        infos = _parse_xml(et, root_ns)
+        infos = _add_url_to_rendererinfo(url, infos)
+        return infos
+    else:
+        _logger.error(f'Error : {ret.status_code}')
 
 
 def set_content_uri(url: str, item_url: str):
@@ -60,11 +120,20 @@ def stop(url: str):
 
 def _main():
     p = argparse.ArgumentParser()
-    p.add_argument('url', help='renderer url')
+    p.add_argument('url', help='renderer url (rederer description URL for get_renderinfo\nAVControl URL for contents play')
     p.add_argument('action', help='action to be called ("set" or "play" or "pause" or "stop")')
     p.add_argument('--item_url', help='item url (needed for action "set"')
     args = p.parse_args()
-    if args.action == 'set':
+    if args.action == 'get_rendererinfo':
+        infos = get_rendererinfo(args.url)
+        for i, info in enumerate(infos):
+            renderer_url = info.get('url')
+            if renderer_url:
+                _logger.info(f'{i} renderURL: {renderer_url}')
+            service_type = info.get('serviceType')
+            if service_type:
+                _logger.info(f'{i} serviceType: {service_type}')
+    elif args.action == 'set':
         set_content_uri(args.url, args.item_url)
     elif args.action == 'play':
         play(args.url)
